041491ed484738c630a169a9961cedab8fe5afca
Philippe Perret <philippe.perret@yahoo.fr>
2020-05-29 05:48:55 +0200

    Girophare

--- _lib/pages/errors/systemique.rb
+++ _lib/pages/errors/systemique.rb
@@ -4,7 +4,7 @@
 =end
 STDOUT.write "Content-type: text/html; charset: utf-8;\n\n"
 STDOUT.write "<div style='padding:3em;font-size:15.2pt;color:red;'>"
-STDOUT.write "<div style='margin-bottom:2em'>⚡ #{ERROR.message.gsub(/</,'&lt;')}</div>"
+STDOUT.write "<div style='margin-bottom:2em'>🚨 #{ERROR.message.gsub(/</,'&lt;')}</div>"
 STDOUT.write ERROR.backtrace.collect{|m| "<div>#{m.gsub(/</,'&lt;')}</div>"}.join('')
 STDOUT.write '</div>'
 STDOUT.write '<p><a href="home">Retourner à l’accueil</p>'
