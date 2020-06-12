dc898a0ec76862c471b2484ea47ba64b9b97e807
Philippe Perret <philippe.perret@yahoo.fr>
2020-06-12 08:44:12 +0200

    Création du watcher pour la définition du partage

---
+++ _lib/_watchers_processus_/IcEtape/qdd_sharing/main.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+class Watcher < ContainerClass
+  def qdd_sharing
+    message "Je dois jouer le processus IcEtape/qdd_sharing"
+  end # / qdd_sharing
+  def contre_qdd_sharing
+    message "Je dois jouer le contre processus IcEtape/contre_qdd_sharing"
+  end # / contre_qdd_sharing
+end # /Watcher < ContainerClass
