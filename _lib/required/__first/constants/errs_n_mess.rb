c5cbf7a65c840694773d197b3a972326a4394108
Philippe Perret <philippe.perret@yahoo.fr>
2020-06-24 05:57:15 +0200

    Pour ne pas répéter la déclaration (avec les tests)

--- _lib/required/__first/constants/errs_n_mess.rb
+++ _lib/required/__first/constants/errs_n_mess.rb
@@ -2,8 +2,10 @@
 =begin
   Constantes pour les messages d'erreur courants
 =end
+ERRORS    = {} unless defined?(ERRORS) # cas des tests
+MESSAGES  = {} unless defined?(MESSAGES) # cas des tests
 
-ERRORS = {
+ERRORS.merge!({
   unfound_user_with_id: "Impossible de trouver un utilisateur d'identifiant %s…".freeze,
   alert_intrusion: "Ceci ressemble à une intrusion en force. Je ne peux pas vous laisser passer…".freeze,
   no_data_modified: 'Aucune donnée n’a été modifiée…'.freeze,
@@ -11,8 +13,8 @@ ERRORS = {
   unfound_data: 'Données introuvables (avec %{with})'.freeze,
   file_unfound: 'Fichier introuvable : %s'.freeze,
   mark_unfound_file: '[FICHIER MANQUANT : `%s`]'.freeze
-}
+})
 
-MESSAGES = {
+MESSAGES.merge!({
   ask_identify: 'Merci de vous identifier avant de rejoindre cette page.'.freeze
-}
+})
