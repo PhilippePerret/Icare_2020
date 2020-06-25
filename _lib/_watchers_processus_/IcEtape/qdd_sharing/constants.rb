75d0e1a6fe40005c2d0588e3e02636d3aa333155
Philippe Perret <philippe.perret@yahoo.fr>
2020-06-25 06:56:15 +0200

    Isolation des constantes pour qu'elles soient publiques

---
+++ _lib/_watchers_processus_/IcEtape/qdd_sharing/constants.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+=begin
+  Constantes (publiques)
+
+  publiques => chargées par les tests
+  
+=end
+DATA_SHARING = {
+  1 => {name:"Partager ce document".freeze},
+  2 => {name:'Ne pas le partager'.freeze}
+}
