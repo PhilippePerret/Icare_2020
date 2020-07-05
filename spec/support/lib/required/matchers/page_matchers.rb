6108b7fbc044e60712abca4927414d7272a048e6
Philippe Perret <philippe.perret@yahoo.fr>
2020-07-05 11:08:03 +0200

    Correction bug quand la page n'a pas du tout de titre

--- spec/support/lib/required/matchers/page_matchers.rb
+++ spec/support/lib/required/matchers/page_matchers.rb
@@ -6,34 +6,39 @@
 RSpec::Matchers.define :have_titre do |expected, options|
   match do |page|
     @errors = []
-    @actual = page.find('h2.page-title').text
-    expected = Regexp.new(expected) if expected.is_a?(String)
-    ok = @actual =~ expected
-    ok || @errors << "le titre devrait être à peu près “#{expected.source}”, mais c’est “#{@actual}”"
-    unless options.nil?
-      if options.key?(:retour)
-        retour_exists = page.has_css?("h2.page-title a[href=\"#{options[:retour][:route]}\"]", text: options[:retour][:text])
-        unless retour_exists
-          if page.has_css?('h2.page-title a')
-            dretour = options[:retour]
-            if dretour.key?(:route)
-              unless page.has_css?("h2.page-title a[href=\"#{dretour[:route]}\"]")
-                hrefretour = page.find('h2.page-title a')['href']
-                @errors << "La route du lien retour devrait être `#{dretour[:route]}`, or c'est `#{hrefretour}`"
+    if page.has_css?('h2.page-title')
+      @actual = page.find('h2.page-title').text
+      expected = Regexp.new(expected) if expected.is_a?(String)
+      ok = @actual =~ expected
+      ok || @errors << "le titre devrait être à peu près “#{expected.source}”, mais c’est “#{@actual}”"
+      unless options.nil?
+        if options.key?(:retour)
+          retour_exists = page.has_css?("h2.page-title a[href=\"#{options[:retour][:route]}\"]", text: options[:retour][:text])
+          unless retour_exists
+            if page.has_css?('h2.page-title a')
+              dretour = options[:retour]
+              if dretour.key?(:route)
+                unless page.has_css?("h2.page-title a[href=\"#{dretour[:route]}\"]")
+                  hrefretour = page.find('h2.page-title a')['href']
+                  @errors << "La route du lien retour devrait être `#{dretour[:route]}`, or c'est `#{hrefretour}`"
+                end
               end
-            end
-            if dretour.key?(:text)
-              unless page.has_css?('h2.page-title a', text: dretour[:text])
-                textretour = page.find('h2.page-title a').text
-                @errors << "Le texte du lien retour devrait être “#{dretour[:text]}” or c'est “#{textretour}”"
+              if dretour.key?(:text)
+                unless page.has_css?('h2.page-title a', text: dretour[:text])
+                  textretour = page.find('h2.page-title a').text
+                  @errors << "Le texte du lien retour devrait être “#{dretour[:text]}” or c'est “#{textretour}”"
+                end
               end
+            else
+              @errors << "le titre ne contient aucun lien retour"
             end
-          else
-            @errors << "le titre ne contient aucun lien retour"
           end
+          ok = ok && retour_exists
         end
-        ok = ok && retour_exists
       end
+    else # Pas de titre
+      @errors << "La page n'a pas de titre…"
+      ok = false
     end
     ok
   end
