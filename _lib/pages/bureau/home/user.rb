e4ab4f94e15889c08630a5cf8fa9dbe82b9ab5ce
Philippe Perret <philippe.perret@yahoo.fr>
2020-07-06 09:33:43 +0200

    Minor changes (occasionnés par le bug précédent)

--- _lib/pages/bureau/home/user.rb
+++ _lib/pages/bureau/home/user.rb
@@ -5,7 +5,7 @@ class User
   end
   def nombre_messages_frigo_non_vus
     @nombre_messages_frigo_non_vus ||= begin
-      request = <<-SQL
+      request = <<-SQL.strip.freeze
       SELECT
         COUNT(mes.id)
         FROM `frigo_messages` AS mes
@@ -14,8 +14,12 @@ class User
           fu.user_id = #{id}
           AND mes.created_at > fu.last_checked_at
       SQL
-      res = db_exec(request)[0]
-      log("res: #{res.inspect}")
+      res = db_exec(request)
+      if MyDB.error
+        log(MyDB.error.inspect)
+        erreur('ERREUR SQL - Consulter le journal d’erreurs.')
+      end
+      res = res.first
       if res.nil? then 0 else res.values.first end
     end
   end #/ nombre_messages_frigo_non_vus
