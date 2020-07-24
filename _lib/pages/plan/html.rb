64054597798741fc1835db03db933d5638e48ef4
Philippe Perret <philippe.perret@yahoo.fr>
2020-07-24 06:24:21 +0200

    Correction émojis du plan

--- _lib/pages/plan/html.rb
+++ _lib/pages/plan/html.rb
@@ -28,26 +28,26 @@ class HTML
 
 #{aGoto('<a href="user/profil">'+user.visage+' Profil</a>'.freeze) unless user.guest?}
 
-#{aGoto('<a href="overview/home">'+EMO_TERRE+' L’ATELIER</a>'.freeze)}
+#{aGoto('<a href="overview/home">'+Emoji.get('nature/terre').regular+' L’ATELIER</a>'.freeze)}
 
-#{aGoto('<a href="overview/reussites">'+EMO_POUCEUP+' Belles réussites</a>'.freeze)}
+#{aGoto('<a href="overview/reussites">'+Emoji.get('gestes/pouceup').regular+' Belles réussites</a>'.freeze)}
 
-#{aGoto('<a href="modules/home">'+EMO_BOITE_DOSSIER+' Tous les modules</a>'.freeze, exergue:true)}
+#{aGoto('<a href="modules/home">'+Emoji.get('objets/boite-dossier').regular+' Les modules</a>'.freeze, exergue:true)}
 
-#{aGoto('<a href="user/signup">'+EMO_FICHIER_CRAYON+' S’inscrire</a>'.freeze, exergue:true) if user.guest?}
-#{aGoto('<a href="user/login">'+EMO_CADENAS_CLE+' S’identifier</a>'.freeze) if user.guest?}
+#{aGoto('<a href="user/signup">'+Emoji.get('objets/fichier-crayon').regular+' S’inscrire</a>'.freeze, exergue:true) if user.guest?}
+#{aGoto('<a href="user/login">'+Emoji.get('objets/cadenas-cle').regular+' S’identifier</a>'.freeze) if user.guest?}
 
-#{aGoto('<a href="overview/icariens">'+EMO_FILLE_ROUSSE_CARREE+EMO_HOMME_MARRON_MOUSTACHE+EMO_FEMME_VOILEE+EMO_EXTRATERRESTRE+EMO_HOMME_BARBE_NOIRE+EMO_JEUNE_HOMME_BLOND+'</a>'.freeze)}
+#{aGoto(('<a href="overview/icariens">'+Emoji.get('humain/fille-rousse-carre').regular+Emoji.get('humain/homme-marron-moustache').regular+Emoji.get('humain/femme-voilee').regular+Emoji.get('humain/extraterrestre').regular+Emoji.get('humain/homme-barbe-noire').regular+Emoji.get('humain/jeune-homme-blond').regular+'</a>').freeze)}
 
-#{aGoto('<a href="contact/mail">'+EMO_LETTRE_MAIL+' Contact</a>'.freeze)}
+#{aGoto('<a href="contact/mail">'+Emoji.get('objets/lettre-mail').regular+' Contact</a>'.freeze)}
 
-#{aGoto('<a href="qdd/home">'+EMO_PILE_LIVRES+' Quai de docs</a>'.freeze)}
+#{aGoto('<a href="qdd/home">'+Emoji.get('objets/pile-livres').regular+' Quai de docs</a>'.freeze)}
 
 #{divGoto(MAIN_LINKS[:aide])}
 
-#{divGoto('<a href="overview/temoignages">'+EMO_TABLEAU_SOLEIL+' Témoignages</a>'.freeze)}
+#{divGoto('<a href="overview/temoignages">'+Emoji.get('objets/tableau-soleil').regular+' Témoignages</a>'.freeze)}
 
-#{divGoto('<a href="overview/activity">'+EMO_SABLIER_COULE+' Activité</a>'.freeze)}
+#{divGoto('<a href="overview/activity">'+Emoji.get('objets/sablier-coule').regular+' Activité</a>'.freeze)}
 
     HTML
   end
