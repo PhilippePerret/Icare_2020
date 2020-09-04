0bd2dab977438528f76a0f678dbbb7d068a85c05
Philippe Perret <philippe.perret@yahoo.fr>
2020-09-04 05:24:09 +0200

    Amélioration du responsive design (à poursuivre)

--- index.rb
+++ index.rb
@@ -1,4 +1,4 @@
-#!/Users/philippeperret/.rbenv/versions/2.6.3/bin/ruby
+#!/usr/bin/ruby
 # encoding: UTF-8
 begin
   require './_lib/required'
@@ -6,9 +6,14 @@ begin
   # Note : Le programme ne passera jamais par ici
 rescue Exception => e
   ERROR = e
-  File.open('./fatal_errors.log','a') do |f|
+  File.open('./fatal_errors.log','wb') do |f|
+    f.puts "Ruby version : #{Gem.ruby_version.version}"
+    f.puts "Gem dir : #{Gem.dir}"
+    f.puts "Gem default_dir : #{Gem.default_dir}"
     f.write("#{Time.now} --- FATAL ERROR:\n    #{e.message}\n    #{e.backtrace.join("\n    ")}")
   end rescue nil
   send_error(e) rescue nil
   require './_lib/pages/errors/systemique'
 end
+###############!/usr/bin/env ruby
+##########!/Users/philippeperret/.rbenv/versions/2.6.3/bin/ruby
