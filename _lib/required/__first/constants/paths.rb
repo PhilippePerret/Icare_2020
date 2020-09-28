6c5f4ac4010af7c399b0309ee4c2ec96538138d6
Philippe Perret <philippe.perret@yahoo.fr>
2020-09-28 11:44:26 +0200

    Ajout du frozen string

--- _lib/required/__first/constants/paths.rb
+++ _lib/required/__first/constants/paths.rb
@@ -1,21 +1,22 @@
 # encoding: UTF-8
+# frozen_string_literal: true
 =begin
   Constantes
 
   Note : d'autres constante peuvent être définies dans le fichier /config.rb
 =end
 APP_FOLDER      = File.dirname(LIB_FOLDER) unless defined?(APP_FOLDER)
-PAGES_FOLDER    = File.join(LIB_FOLDER,'_pages_'.freeze) unless defined?(PAGES_FOLDER)
+PAGES_FOLDER    = File.join(LIB_FOLDER,'_pages_') unless defined?(PAGES_FOLDER)
 FOLD_REL_PAGES = './_lib/_pages_' unless defined?(FOLD_REL_PAGES)
-PROCESSUS_WATCHERS_FOLDER = File.join(LIB_FOLDER,'_watchers_processus_'.freeze)
-DATA_FOLDER     = File.join(LIB_FOLDER,'data'.freeze) unless defined?(DATA_FOLDER)
-MODULES_FOLDER  = File.join(LIB_FOLDER,'modules'.freeze)
-TEMP_FOLDER     = File.join(APP_FOLDER,'tmp'.freeze)
-FORMS_FOLDER    = File.join(TEMP_FOLDER,'forms'.freeze)
-LOGS_FOLDER     = File.join(TEMP_FOLDER,'logs'.freeze)
-DOWNLOAD_FOLDER = File.join(TEMP_FOLDER,'downloads'.freeze)
-QDD_FOLDER      = File.join(DATA_FOLDER, 'qdd'.freeze)
-PUBLIC_FOLDER   = File.join(APP_FOLDER, 'public'.freeze)
+PROCESSUS_WATCHERS_FOLDER = File.join(LIB_FOLDER,'_watchers_processus_')
+DATA_FOLDER     = File.join(LIB_FOLDER,'data') unless defined?(DATA_FOLDER)
+MODULES_FOLDER  = File.join(LIB_FOLDER,'modules')
+TEMP_FOLDER     = File.join(APP_FOLDER,'tmp')
+FORMS_FOLDER    = File.join(TEMP_FOLDER,'forms')
+LOGS_FOLDER     = File.join(TEMP_FOLDER,'logs')
+DOWNLOAD_FOLDER = File.join(TEMP_FOLDER,'downloads')
+QDD_FOLDER      = File.join(DATA_FOLDER, 'qdd')
+PUBLIC_FOLDER   = File.join(APP_FOLDER, 'public')
 
 # Les dossiers à construire, le cas échéant
 # Note : on pourrait supprimer ces lignes après un certain temps
