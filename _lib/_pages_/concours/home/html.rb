02d93fcf7080df361cc1897efed458bc7f8bd40f
Philippe Perret <philippe.perret@yahoo.fr>
2020-10-02 06:33:33 +0200

    Modification du titre de page

--- _lib/_pages_/concours/home/html.rb
+++ _lib/_pages_/concours/home/html.rb
@@ -1,18 +1,19 @@
 # encoding: UTF-8
+# frozen_string_literal: true
 require_module('form')
 class HTML
   def titre
-    "Concours".freeze
+    "Le Concours de l’atelier Icare"
   end #/titre
-  
+
   # Code à exécuter avant la construction de la page
   def exec
-    
+
   end # /exec
 
   # Fabrication du body
   def build_body
-    @body = deserb(STRINGS[:body], self)
+    @body = deserb('body', self)
   end # /build_body
 
 end #/HTML
