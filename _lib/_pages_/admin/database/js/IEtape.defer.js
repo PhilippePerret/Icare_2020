7f1f73fc1928785f022764f66793ec1743a5ee7e
Philippe Perret <philippe.perret@yahoo.fr>
2020-10-13 14:52:41 +0200

    Poursuite implémentation des propriétés propres

--- _lib/_pages_/admin/database/js/IEtape.defer.js
+++ _lib/_pages_/admin/database/js/IEtape.defer.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const DATA_ETAPES_STATUS = {
-  1: "L’icarien travaille dessus",
-  2: "L’icarien vient de transmettre son travail",
-  3: "Je commente le travail de L’icarien",
-  4: "Je viens de transmettre mes commentaires",
-  5: "L’icarien a chargé les commentaires",
-  6: "Les documents ont été déposé sur le QdD",
-  7: "L’icarien a défini le partage de ses documents",
-  8: "Étape complètement achevée"
+  1: {text:"1 : L’icarien travaille dessus"},
+  2: {text:"2 : L’icarien vient de transmettre son travail"},
+  3: {text:"3 : Je commente le travail de L’icarien"},
+  4: {text:"4 : Je viens de transmettre mes commentaires"},
+  5: {text:"5 : L’icarien a chargé les commentaires"},
+  6: {text:"6 : Les documents ont été déposé sur le QdD"},
+  7: {text:"7 : L’icarien a défini le partage de ses documents"},
+  8: {text:"8 : Étape complètement achevée"}
 }
 
 class IEtape extends Objet {
@@ -53,53 +53,28 @@ build_all_own_data(){
   * Le statut correspond
 ***/
 get human_status(){
-  this.constructor.menuStatuts // pour forcer sa construction
-  const vh = DATA_ETAPES_STATUS[this.data.status]
-  const text = `${this.data.status} ${vh}`
-  const span = DCreate('SPAN', {text:text, class:"linked"})
-  const Classe = this.constructor
-  span.addEventListener('click', Classe.openMenuStatutsAndChoose.bind(Classe, this.objet))
-  return span
+  var m = new DSelect({
+    id:'status',
+    default_value:this.data.status,
+    onchange: this.onChangeStatus.bind(this),
+    values:   DATA_ETAPES_STATUS
+  })
+  return m.menu
 }
-static openMenuStatutsAndChoose(ietape, ev){
-  this.onChangeStatut.ietape = ietape
-  const menu = this.menuStatuts
-  menu.classList.remove('hidden')
-  menu.style.top = `${ev.y - 10}px`
-  menu.style.left = `${ev.x - 40}px`
-}
-static onChangeStatut(ev){
-  const ietape = this.onChangeStatut.ietape
-  const newStatus = DGet('#status').value
-  const human_etape = `${ietape.data.numero}. ${ietape.data.titre}`
-  const human_status = `${newStatus} (${DATA_ETAPES_STATUS[newStatus]})`
-  if (confirm(`Dois-je mettre le statut de :\n\n\tl'étape ${human_etape}\nà :\n\t${human_status} ?`)){
+onChangeStatus(newStatus, ev){
+  newStatus = Number(newStatus)
+  const human_status = DATA_ETAPES_STATUS[newStatus].text
+  if (confirm(`Dois-je mettre le statut de :\n\n\tl'étape ${this.as_human}\nà :\n\t${human_status} ?`)){
     ietape.fiche.execCodeUpdate('icetapes',`SET status = ${newStatus}`,{no_confirmation:true})
-    message(`J'ai mis le statut de l'étape ${human_etape} à ${human_status}.`)
+    message(`J'ai mis le statut de l'étape ${this.as_human} à ${human_status}.`)
   }
   this.closeMenuStatuts()
 }
-static closeMenuStatuts(){
-  const menu = this.menuStatuts
-  menu.classList.add('hidden')
+
+get as_human(){
+  return this._ashuman || (this._ashuman = `${this.data.numero}. ${this.data.titre}`)
 }
-static get menuStatuts(){
-  return this._menustatuts||(this._menustatuts = this.buildMenuStatuts())
-}
-static buildMenuStatuts(){
-  const menu = DCreate('SELECT', {
-    id:'status',
-    class:'hidden',
-    style:'position:absolute;z-index:200;background-color:white;'
-  })
-  for (var s in DATA_ETAPES_STATUS){
-    var h = DATA_ETAPES_STATUS[s]
-    menu.appendChild(DCreate('OPTION',{value:s, text:`${s}: ${h}`}))
-  }
-  document.querySelector('body').appendChild(menu)
-  menu.addEventListener('change', this.onChangeStatut.bind(this))
-  return menu
-}
+
 
 /**
  * Construit les données supplémentaires
