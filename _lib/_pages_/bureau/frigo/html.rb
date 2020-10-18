6d53c9099da0aecb1eab38855d8e3a39817f7a25
Philippe Perret <philippe.perret@yahoo.fr>
2020-10-18 15:53:13 +0200

    Correction du bug sur icarien_required

--- _lib/_pages_/bureau/frigo/html.rb
+++ _lib/_pages_/bureau/frigo/html.rb
@@ -12,74 +12,76 @@ class HTML
   def exec
     # Code à exécuter avant la construction de la page
     icarien_required
-    case param(:op)
-    when 'download'
-      # Opération appelée quand on veut télécharger la discussion param(:disid)
-      # Le paramètre :follow peut définir la suite, quand par exemple il faut
-      # détruire la discussion après l'opération.
-      param(:disid) || raise(ERRORS[:discussion_required])
-      FrigoDiscussion.download_discussion(param(:disid))
-      return
-    when 'decliner_invitation'
-      # Méthode appelée depuis un mail pour décliner une invitation à
-      # participer à une discussion
-      param(:did) || raise(ERRORS[:discussion_required]) # passage en force
-      user.quit_discussion(param(:did))
-      return
-    when 'inviter'
-      param(:did) || raise(ERRORS[:discussion_required])
-      owner_discussion_required || user.admin? || raise(ERRORS[:inviter_requires_owner])
-      return
-    when 'destroy'
-      param(:did) || raise(ERRORS[:discussion_required]) # passage en force
-      owner_discussion_required || user.admin? || raise(ERRORS[:destroy_requires_owner])
-      return
-    when 'annonce_destruction'
-      # Méthode appelée quand le possesseur d'une discussion veut la détruire
-      param(:did) || raise(ERRORS[:discussion_required]) # passage en force
-      owner_discussion_required || user.admin? || raise(ERRORS[:destroy_requires_owner])
-      FrigoDiscussion.get(param(:did)).annonce_destruction
-      return
-    when 'quitter_discussion'
-      # Méthode appelée quand l'user clique sur le bouton pour quitter la
-      # discussion sur laquelle il se trouve
-      param(:did) || raise(ERRORS[:discussion_required]) # passage en force
-      user.quit_discussion(param(:did))
-      param(:did, EMPTY_STRING) # Pour le titre
-      return
-    when 'marquer_lus'
-      # On passe par ici quand l'user clique sur le bouton pour marquer une
-      # discussion "à jour" c'est-à-dire qu'il a lu tous les nouveaux messages
-      # cela change la date de son last_checked_at dans frigo_users
-      param(:disid) || raise(ERRORS[:discussion_required]) # passage en force
-      user.marquer_discussion_lue(param(:disid))
-      return
-    when 'send_invitations'
-      # On passe par ici quand l'user a demandé à afficher la liste des
-      # icarien (et administrateurs) pour les inviter à rejoindre une conversa
-      # tion. L'icarien a choisi les icariens et on va leur envoyer une
-      # invitation
-      param(:disid) || raise(ERRORS[:discussion_required]) # passage en force
-      FrigoDiscussion.get(param(:disid)).send_invitations_to(param(:icariens))
-      return
+    begin
+      case param(:op)
+      when 'download'
+        # Opération appelée quand on veut télécharger la discussion param(:disid)
+        # Le paramètre :follow peut définir la suite, quand par exemple il faut
+        # détruire la discussion après l'opération.
+        param(:disid) || raise(ERRORS[:discussion_required])
+        FrigoDiscussion.download_discussion(param(:disid))
+        return
+      when 'decliner_invitation'
+        # Méthode appelée depuis un mail pour décliner une invitation à
+        # participer à une discussion
+        param(:did) || raise(ERRORS[:discussion_required]) # passage en force
+        user.quit_discussion(param(:did))
+        return
+      when 'inviter'
+        param(:did) || raise(ERRORS[:discussion_required])
+        owner_discussion_required || user.admin? || raise(ERRORS[:inviter_requires_owner])
+        return
+      when 'destroy'
+        param(:did) || raise(ERRORS[:discussion_required]) # passage en force
+        owner_discussion_required || user.admin? || raise(ERRORS[:destroy_requires_owner])
+        return
+      when 'annonce_destruction'
+        # Méthode appelée quand le possesseur d'une discussion veut la détruire
+        param(:did) || raise(ERRORS[:discussion_required]) # passage en force
+        owner_discussion_required || user.admin? || raise(ERRORS[:destroy_requires_owner])
+        FrigoDiscussion.get(param(:did)).annonce_destruction
+        return
+      when 'quitter_discussion'
+        # Méthode appelée quand l'user clique sur le bouton pour quitter la
+        # discussion sur laquelle il se trouve
+        param(:did) || raise(ERRORS[:discussion_required]) # passage en force
+        user.quit_discussion(param(:did))
+        param(:did, EMPTY_STRING) # Pour le titre
+        return
+      when 'marquer_lus'
+        # On passe par ici quand l'user clique sur le bouton pour marquer une
+        # discussion "à jour" c'est-à-dire qu'il a lu tous les nouveaux messages
+        # cela change la date de son last_checked_at dans frigo_users
+        param(:disid) || raise(ERRORS[:discussion_required]) # passage en force
+        user.marquer_discussion_lue(param(:disid))
+        return
+      when 'send_invitations'
+        # On passe par ici quand l'user a demandé à afficher la liste des
+        # icarien (et administrateurs) pour les inviter à rejoindre une conversa
+        # tion. L'icarien a choisi les icariens et on va leur envoyer une
+        # invitation
+        param(:disid) || raise(ERRORS[:discussion_required]) # passage en force
+        FrigoDiscussion.get(param(:disid)).send_invitations_to(param(:icariens))
+        return
+      end
+      if param(:form_id)
+        form = Form.new
+        if form.conform?
+          case param(:form_id)
+          when 'discussion-all-form'
+            start_discussion_with_all # je lance une discussion
+          when 'discussion-phil-form'
+            start_discussion_with_phil # un icarien lance une discuss avec moi
+          when 'discussion-form'
+            add_message_to_discussion
+          end
+        end #/fin de si le formulaire est conforme
+      end
+    rescue Exception => e
+      log(e)
+      param(:op, '')
+      erreur(e.message)
     end
-    if param(:form_id)
-      form = Form.new
-      if form.conform?
-        case param(:form_id)
-        when 'discussion-all-form'
-          start_discussion_with_all # je lance une discussion
-        when 'discussion-phil-form'
-          start_discussion_with_phil # un icarien lance une discuss avec moi
-        when 'discussion-form'
-          add_message_to_discussion
-        end
-      end #/fin de si le formulaire est conforme
-    end
-  rescue Exception => e
-    log(e)
-    param(:op, '')
-    erreur(e.message)
   end #/exec
 
   # Construction du corps de la page
