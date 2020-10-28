a7bcc84c5d990d6794a589cfbb1512b488f4a201
Philippe Perret <philippe.perret@yahoo.fr>
2020-10-28 05:45:13 +0100

    Minor change

--- _lib/required.rb
+++ _lib/required.rb
@@ -13,7 +13,7 @@ require 'mysql2'
 require 'erb'
 require 'yaml'
 
-LIB_FOLDER = File.dirname(__FILE__)
+LIB_FOLDER  = File.dirname(__FILE__)
 TESTS       = File.exists?('./TESTS_ON') # réglé par spec_helper.rb
 
 require_relative 'required/__first/constants/paths'
@@ -30,11 +30,11 @@ if not String.respond_to?(:match?)
   Dir["#{LIB_FOLDER}/xtra_old_ruby_versions/**/*.rb"].each{|m|require m}
 end
 
-log("Version ruby #{RUBY_VERSION}")
-log("ONLINE: #{ONLINE.inspect} (OFFLINE est #{OFFLINE.inspect})")
-log("DATABASE: #{MyDB.DBNAME.inspect}")
-log("ENV['REMOTE_ADDR'] = #{ENV['REMOTE_ADDR'].inspect}")
-log("TESTS : #{TESTS.inspect}")
+# log("Version ruby #{RUBY_VERSION}")
+# log("ONLINE: #{ONLINE.inspect} (OFFLINE est #{OFFLINE.inspect})")
+# log("DATABASE: #{MyDB.DBNAME.inspect}")
+# log("ENV['REMOTE_ADDR'] = #{ENV['REMOTE_ADDR'].inspect}")
+# log("TESTS : #{TESTS.inspect}")
 unless SELF_LOADED
   log("ROUTE : #{route.to_s}")
   # On trace ce chargement
