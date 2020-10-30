aff90a93f1d1a1c37c4473ca38bca8977ed69ad9
Philippe Perret <philippe.perret@yahoo.fr>
2020-10-30 19:53:55 +0100

    Test de l'envoi de fichier pour le concours (en cours)

--- spec/features/concours/envoi_fichier_candidature_spec.rb
+++ spec/features/concours/envoi_fichier_candidature_spec.rb
@@ -1,11 +1,35 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
+class TConcurrent
+  # IN    +data+ table des données, doit contenir :
+  #         :titre, :synopsis et optionnellement :auteurs
+  #         Le :synopsis est le path ABSOLU du fichier
+  # DO    Prend le concurrent en tant que visiteur quelconque, l'identifie,
+  #       le conduit à son espace personnel, remplit le formulaire d'envoi
+  #       du synopsis et le soumet.
+  def come_and_send_synopsis(data)
+    identify
+    visit("http://localhost/AlwaysData/Icare_2020/concours/espace_concurrent")
+    within("form#concours-dossier-form") do
+      fill_in("p_titre",    with: data[:titre])
+      fill_in("p_auteurs",  with: data[:auteurs]) if data.key?(:auteurs)
+      attach_file("p_fichier_candidature", data[:synopsis])
+      click_on(UI_TEXTS[:concours_bouton_send_dossier])
+    end
+  end #/ envoi_son_synopsis
+end #/TConcurrent
+
 feature "Dépôt du fichier de candidature" do
 
+
+  before(:all) do
+    require './_lib/_pages_/concours/xrequired/constants'
+    require_support('concours')
+    degel('concours')
+  end
+
   context 'Quand le concours est en route (step 1)' do
-
-
 
     context 'Un visiteur quelconque' do
       scenario 'ne peut pas déposer de fichier de candidature' do
@@ -17,17 +41,28 @@ feature "Dépôt du fichier de candidature" do
 
 
     context 'Un concurrent inscrit' do
-      scenario 'peut déposer son fichier de candidature' do
+      scenario 'peut déposer son fichier de candidature', only:true do
+        # *** Préparation ***
+        concurrent = TConcurrent.get_random(without_synopsis: true)
+        expect(concurrent.specs[0]).not_to eq "1"
+        # *** Test ***
+        syno_path = File.expand_path(File.join('.','spec','support','asset','documents','synopsis_concours.pdf'))
+        concurrent.come_and_send_synopsis(titre: "À plus d'un titre", synopsis: syno_path)
 
-        # *** Vérifications préliminaires ***
+        # *** Vérifications ***
+        # Le document a été déposé avec le bon titre au bon endroit
+        # (vérifier aussi la taille)
+        path = File.join(concurrent.folder, "#{concurrent.id}-#{ANNEE_CONCOURS_COURANTE}.pdf")
+        expect(File).to be_exists(path)
+        expect(File.stat(syno_path).size).to eq(File.stat(path).size)
+        # Un mail de confirmation a été envoyé au concurrent
         # TODO
-
-        # Un mail est envoyé au concurrent pour confirmer son envoi
+        # Les specs de son enregistrement pour le concours ont été modifiée
+        concurrent.reset
+        expect(concurrent.specs[0..1]).to eq "10"
+        # J'ai reçu un mail m'informant de l'envoi du synopsis
         # TODO
-
-        # Un mail est envoyé à l'administration pour confirmer, qui contient
-        # l'auteur et les références du fichier. Contient aussi la commande
-        # pour charger (downloader) le fichier
+        # Une actualité annonce l'envoi du synopsis
         # TODO
       end
     end #/ Context Un concurrent inscrit
--- spec/support/modules/concours/TConcurrent.rb
+++ spec/support/modules/concours/TConcurrent.rb
@@ -1,6 +1,8 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 class TConcurrent
+  include Capybara::DSL
+
 class << self
 
   def reset
@@ -8,9 +10,36 @@ class << self
   end #/ reset
 
   # Retourne une instance TConcurrent choisie au hasard
-  def get_random
-    all[rand(all.count)]
+  #
+  # IN    +options+ Table d'options:
+  #                 :femme    Si true on doit retourner une femme
+  #                 :without_synopsis   Si true, on doit retourner un
+  #                       concurrent sans synopsis.
+  # OUT   Un concurrent pris au hasard, qui peut remplir certaines
+  #       conditions optionnellement définies par +options+.
+  #       Mais c'est forcément un candidat courant
+  #
+  def get_random(options = nil)
+    candidat =  if options[:femme]
+                  get_une_femme
+                else
+                  all_current[rand(all.count)]
+                end
+    # Si on veut un candidat sans synopsis
+    if options[:without_synopsis] && File.exists?(candidat.folder)
+      # On doit détruire son dossier
+      FileUtils.rm_rf(candidat.folder)
+      # On doit régler ses options (specs à "00000000")
+      candidat.set_specs("0"*8)
+    end
+    return candidat
   end #/ get_a_concurrent
+
+  def get_une_femme
+    all.each do |concurrent|
+      return concurrent if concurrent.femme?
+    end
+  end #/ get_femme
 
   def all
     @allconcurrents ||= begin
@@ -72,16 +101,46 @@ end # /<< self
 #
 # ---------------------------------------------------------------------
 attr_reader :patronyme, :mail, :concurrent_id, :options, :created_at, :updated_at
+attr_reader :specs, :titre, :auteurs, :keywords, :prix
 def initialize(data)
-  data.each{|k,v|instance_variable_set("@#{k}",v)}
+  # puts "Data : #{data.inspect}"
+  dispatch(data)
 end #/ initialize
 
 alias :pseudo :patronyme
 alias :id :concurrent_id
 
+def reset
+  dispatch(db_exec(REQUEST_CONCURRENT_ALL_DATA, [ANNEE_CONCOURS_COURANTE, id]).first)
+end #/ reset
+
+def dispatch(d)
+  d.each{|k,v|instance_variable_set("@#{k}",v)}
+end #/ dispatch
+
+# Méthode de feature pour identifier le concurrent
+# Il rejoint le formulaire d'identification, le remplit et le soumet
+def identify
+  visit("http://localhost/AlwaysData/Icare_2020/concours/identification")
+  within("form#concours-login-form") do
+    fill_in("p_mail", with: mail)
+    fill_in("p_concurrent_id", with:id)
+    click_on(UI_TEXTS[:concours_bouton_sidentifier])
+  end
+end #/ identify
+
+def set_specs(new_specs)
+  request = "UPDATE concurrents_per_concours SET specs = ? WHERE concurrent_id = ? AND annee = ?"
+  db_exec(request, [new_specs, id, ANNEE_CONCOURS_COURANTE])
+end #/ set_specs
+
 def folder
   @folder ||= File.join(CONCOURS_DATA_FOLDER, self.id)
 end #/ folder
+
+def femme?
+  sexe == 'F'
+end #/ femme?
 
 REQUEST_CONCURRENTS_COURANTS = <<-SQL
 SELECT
@@ -89,5 +148,13 @@ SELECT
   FROM concours_concurrents cc
   INNER JOIN concurrents_per_concours cpc ON cc.concurrent_id = cpc.concurrent_id
   WHERE cpc.annee = ?
+SQL
+
+REQUEST_CONCURRENT_ALL_DATA = <<-SQL
+SELECT
+  cc.*, cpc.titre, cpc.auteurs, cpc.keywords, cpc.specs, cpc.prix
+  FROM concours_concurrents cc
+  INNER JOIN concurrents_per_concours cpc ON cc.concurrent_id = cpc.concurrent_id
+  WHERE cpc.annee = ? AND cc.concurrent_id = ?
 SQL
 end #/TConcurrent
