e5d75f29f03c822d3fc065c00055589472d8b51f
Philippe Perret <philippe.perret@yahoo.fr>
2020-11-01 11:45:40 +0100

    Fixed #399 (destruction profil)

--- _lib/_pages_/user/destroy/user.rb
+++ _lib/_pages_/user/destroy/user.rb
@@ -1,11 +1,11 @@
 # encoding: UTF-8
+# frozen_string_literal: true
 class User
   # = main =
   # Méthode principale de destruction du l'utilisateur courant
   def destroy
     # On anonymise ses éléments (user#9)
-    tmp_request = "UPDATE `%{table}` SET user_id = 9 WHERE user_id = #{id}".freeze
-
+    tmp_request = "UPDATE `%{table}` SET user_id = 9 WHERE user_id = #{id}"
     [
       'actualites', 'icdocuments', 'icetapes', 'icmodules',
       'frigo_discussions', 'frigo_messages',
@@ -15,19 +15,37 @@ class User
     end
 
     # Destruction dans les discussions de frigo
-    request_owner = "UPDATE `frigo_discussions` SET user_id = 9 WHERE user_id = #{id}".freeze
+    request_owner = "UPDATE `frigo_discussions` SET user_id = 9 WHERE user_id = #{id}"
     db_exec(request_owner)
 
     # On détruit les éventuels tickets en cours
-    request_delete = "DELETE FROM %{table} WHERE user_id = #{id}".freeze
+    request_delete = "DELETE FROM %{table} WHERE user_id = #{id}"
     [
       'tickets', 'watchers'
     ].each do |table|
       db_exec(request_delete % {table: table})
     end
 
+    # On détruit toutes ses éventuelles participations aux concours
+    begin
+      log("--> Destruction des participations éventuelles aux concours")
+      dc = db_exec("SELECT concurrent_id FROM concours_concurrents WHERE mail = ?", [mail]).first
+      unless dc.nil?
+        concurrent_id = dc[:concurrent_id]
+        req_destroy_cpc = "DELETE FROM concurrents_per_concours WHERE concurrent_id = ?"
+        db_exec(req_destroy_cpc, [concurrent_id])
+        req_destroy_cc = "DELETE FROM concours_concurrents WHERE concurrent_id = ?"
+        db_exec(req_destroy_cc, [concurrent_id])
+        log("   Destruction des participations effectuée avec succès")
+      else
+        log("   Aucune participation n'a été trouvée")
+      end
+    rescue Exception => e
+      log(e)
+    end
+
     # Et enfin dans la table users, on le détruit
-    db_exec("DELETE FROM `users` WHERE id = #{id}".freeze)
+    db_exec("DELETE FROM `users` WHERE id = #{id}")
 
     # On le déconnecte complètement
     deconnexion
--- spec/features/user/destroy_user_spec.rb
+++ spec/features/user/destroy_user_spec.rb
@@ -17,7 +17,7 @@ feature "Destruction d'un icarien" do
       "Un non icarien devrait être conduit au formulaire d'identification"
   end
 
-  scenario 'Une icarienne peut détruire son profil', only:true do
+  scenario 'Une icarienne peut détruire son profil' do
     extend SpecModuleMarion
     degel('envoi_travail')
     identify_marion
@@ -26,5 +26,11 @@ feature "Destruction d'un icarien" do
     click_on UI_TEXTS[:btn_detruire_profil]
     expect(page).to have_selector('h2', text:'Destruction du profil')
     # TODO CONTINUER DE TESTER JUSQU'À LA FIN
+  end
+
+  context 'Une icarienne qui a participé à une session de concours' do
+    scenario 'perd également toutes les informations sur le concours' do
+      implementer(__FILE__,__LINE__)
+    end
   end
 end
