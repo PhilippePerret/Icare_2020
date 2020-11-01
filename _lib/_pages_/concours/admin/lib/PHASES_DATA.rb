1382898ae8692f33b05e9a3ca8ab49b0a57e5ca2
Philippe Perret <philippe.perret@yahoo.fr>
2020-11-01 06:17:46 +0100

    Changement de STEP_DATA pour PHASES_DATA

---
+++ _lib/_pages_/concours/admin/lib/PHASES_DATA.rb
@@ -0,0 +1,50 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+class Concours
+PHASES_DATA = {
+  0 => {name: "En attente", name_current:"En attente", name_done: "Préparé"},
+  1 => {name: "Lancer et annoncer le concours", name_current:"Lancement du concours en cours", name_done: "Concours lancé et annoncé"},
+  2 => {name: "Statuer l'échéance des rendus", name_current: "Première sélection en cours", name_done: "Première sélection effectuée"},
+  3 => {name: "Annoncer première sélection", name_current: "Seconde sélection en cours", name_done: "Sélection finale effectuée"},
+  5 => {name: "Annoncer le palmarès", name_current:"Annonce du palmarès en cours", name_done: "Palmarès annoncé"},
+  8 => {name: "Annoncer fin officielle du concours", name_current:"Annonce de la fin du concours", name_done: "Fin officielle du concours"},
+  9 => {name: "Nettoyer le concours", name_current:"Nettoyage du concours en cours", name_done:"Concours nettoyé"}
+}
+PHASES_DATA[0].merge!(operations:[
+  {name:"Le concours ne peut s'atteindre que depuis le plan", info:true},
+  {name:"Un panneau minimal permet de s'inscrire à la prochaine session et de lire le règlement", info:true}
+])
+PHASES_DATA[1].merge!(operations: [
+  {name:"Présence de la “pub” en bas à gauche des premières pages", info:true},
+  {name:"Accueil : affichage du panneau avec le nombre d'inscrits, le thème et l'échéance etc.", info:true},
+  {name:"Espace personnel : possibilité d'envoyer son fichier", info:true},
+  {name:"📤 Envoi du mail d'annonce de lancement à tous les icariens", method: :send_mail_icariens_annonce_start},
+  {name:"📤 Envoi du mail d'annonce de lancement à tous les concurrents", method: :send_mail_concurrents_annonce_start},
+  {name:"📤 Envoi du mail d'annonce de lancement à tous les membres du jury", method: :send_mail_jury_annonce_start},
+  {name:"📣 Actualité annonçant l'ouverture du concours", method: :add_actualite_concours_start}
+])
+PHASES_DATA[2].merge!(operations:[
+  {name:"📤 Envoi du mail aux concurrents annonçant la fin de l’échéance"},
+  {name:"📤 Envoi du mail aux jurés annonçant la fin de l'échéance"},
+  {name:"Retrait du formulaire pour envoyer son dossier", info: true},
+])
+PHASES_DATA[3].merge!(operations:[
+  {name:"📤 Envoi du mail aux concurrents annonçant les résultats de la pré-sélection"},
+  {name:"Panneau dans la section “Résultats” pour voir les pré-sélections", info: true}
+])
+PHASES_DATA[5].merge!(operations:[
+  {name:"📤 Envoi du mail aux concurrent annonçant le palmarès final"},
+  {name:"Construction du panneau pour voir les résultats finaux"},
+  {name:"Construction des fiches de lecture de chaque concurrent"},
+  {name:"Affichage de la fiche de lecture sur l'espace personnel"}
+])
+PHASES_DATA[8].merge!(operations:[
+  {name:"📤 Envoi du mail de remerciement (et félicitations) à tous concurrents"},
+  {name:"📤 Envoi du mail de remerciement aux jurés"},
+  {name:"Le concours n'est plus annoncé sur l'atelier"}
+])
+PHASES_DATA[9].merge!(operations:[
+  {name:"Mise des dossiers de côté (zippés)"}
+])
+
+end #/Concours
