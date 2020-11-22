8bc06cf1fa09f771fe24472814d1f5f3c0b679cb
Philippe Perret <philippe.perret@yahoo.fr>
2020-11-22 14:27:16 +0100

    Poursuite des tests sur les phases du concours

--- spec/features/concours/phases/phase2/phase2_evaluators_spec.rb
+++ spec/features/concours/phases/phase2/phase2_evaluators_spec.rb
@@ -11,23 +11,29 @@ require_relative '../phase1/it_cases.rb'
 
 feature "En PHASE 2" do
   before(:all) do
-    # headless()
     degel('concours-phase-2')
     require './_lib/_pages_/concours/evaluation/lib/constants'
     DATA_QUESTIONS = YAML.load_file('./_lib/_pages_/concours/evaluation/data/data_evaluation.yaml')
-    # puts "DATA_QUESTIONS: #{DATA_QUESTIONS}"
-    @concurrent = TConcurrent.find(avec_fichier_conforme: true).shuffle.shuffle.first
-    @member = TEvaluator.get_random(fiche_evaluation: @concurrent, jury: 1)
   end
   let(:member) { @member }
   let(:fiche_evaluation) { @fiche_evaluation ||= member.fiche_evaluation(concurrent) }
   let(:concurrent) { @concurrent }
   let(:synopsis_id) { @synopsis_id = "#{concurrent.id}-#{annee}" }
-  let(:annee) { ANNEE_CONCOURS_COURANTE }
 
-  context 'un évaluateur (aka membre du jury)' do
-    it { peut_modifier_son_evaluation }
+  context 'un évaluateur du jury 1 (aka membre du jury)' do
+    before :all do
+      @concurrent = TConcurrent.find(avec_fichier_conforme: true).shuffle.shuffle.first
+      @member = TEvaluator.get_random(fiche_evaluation: @concurrent, jury: 1)
+    end
     it { peut_modifier_son_evaluation }
     it { peut_evaluer_un_synopsis_par_le_minichamp }
+  end
+
+  context 'un évaluator du second jury' do
+    before :all do
+      @concurrent = TConcurrent.find(avec_fichier_conforme: true).shuffle.shuffle.first
+      @member = TEvaluator.get_random(fiche_evaluation: @concurrent, jury: 2)
+    end
+    it { ne_peut_pas_atteindre_la_section_evalutation }
   end
 end
