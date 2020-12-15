f7dabfee6bb2baa43fd2de8fd20c8687d135190c
Philippe Perret <philippe.perret@yahoo.fr>
2020-12-15 05:34:36 +0100

    Ajout du manuel cronjob et traitement des choices pour tty-prompt

--- _dev_/CLI/lib/commands/write.rb
+++ _dev_/CLI/lib/commands/write.rb
@@ -11,6 +11,8 @@ MESSAGES.merge!({
 DATA_WHAT_WRITE = [
   {name:'Manuel Atelier', value: :manuel},
   {name:'Manuel Concours', value: :concours},
+  {name:'Manuel Cronjob', value: :cronjob},
+  {name:'Renoncer', value: nil}
 ]
 
 SSH_SERVER = 'icare@ssh-icare.alwaysdata.net'
@@ -22,11 +24,11 @@ class << self
     what = params[1]
     unless self.respond_to?("write_#{what}".to_sym)
       what = Q.select(MESSAGES[:question_write], required: true) do |q|
-        q.choices DATA_WHAT_WRITE.collect{|d| d.merge(name:"#{d[:name]} [#{d[:value]}]")}
+        q.choices formate_choices('write', DATA_WHAT_WRITE)
         q.per_page DATA_WHAT_WRITE.count
       end
     end
-    return if what == :cancel
+    return if what.nil? || what == :cancel
     self.send("write_#{what}".to_sym)
   end #/ proceed_write
 
@@ -38,6 +40,10 @@ class << self
   def write_concours
     `open -a Typora "#{File.join(FOLD_REL_PAGES,'concours','_Manuel_Concours_.md')}"`
   end #/ write_concours
+
+  def write_cronjob
+    `open -a Typora "#{File.join(APP_FOLDER,'cronjob2','_Manuel_Cronjob_.md')}"`
+  end #/ write_cronjob
 
 end # /<< self
 end #/IcareCLI
